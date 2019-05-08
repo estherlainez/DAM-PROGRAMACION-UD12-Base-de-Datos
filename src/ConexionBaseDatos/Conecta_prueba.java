871b29b6c695d0141c539269a1f113936d4f9c5c
Esther Lainez <esthertarazona81@gmail.com>
2019-05-08 23:00:48 +0200

    first commit

---
+++ src/ConexionBaseDatos/Conecta_prueba.java
@@ -0,0 +1,28 @@
+package ConexionBaseDatos;
+import java.sql.*;
+public class Conecta_prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		try {
+			//1. Crear conexion
+			Connection miConexion= DriverManager.getConnection("jdbc:mysql://localhost:3306/clientes");
+			
+			//2.Crear objeto statement
+			Statement miStatement= miConexion.createStatement();
+			
+			//3. Ejecutar sql
+			ResultSet miResulset=miStatement.executeQuery("SELET * FROM CLIENTES");
+			
+			//4. Recorrer el Resultset
+				while(miResulset.next()) {
+					System.out.println(miResulset.getString("nombre")+" "+miResulset.getString("id")+" "+miResulset.getString("telefono"));
+				}
+		}catch(Exception e) {
+			System.out.println("No conecta");
+			e.printStackTrace();
+		}
+	}
+
+}
